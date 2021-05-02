affad7b336ed7c57157461be3b8499666f6c97dc
RachmanForniandi <rachmanforniandi@gmail.com>
2021-05-02 18:16:16 +0700

    added navigation component done

--- app/src/main/java/rachmanforniandi/celestialnews/ui/activity/MainActivity.kt
+++ app/src/main/java/rachmanforniandi/celestialnews/ui/activity/MainActivity.kt
@@ -1,7 +1,11 @@
-package rachmanforniandi.celestialnews
+package rachmanforniandi.celestialnews.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import rachmanforniandi.celestialnews.R
 import rachmanforniandi.celestialnews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,9 +13,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bottomNavNews.setupWithNavController(fragment.findNavController())
     }
 }
