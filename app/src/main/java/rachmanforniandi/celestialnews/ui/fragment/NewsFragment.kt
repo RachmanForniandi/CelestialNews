c073c245821389f8718171bec0a19208e7b81f31
RachmanForniandi <rachmanforniandi@gmail.com>
2021-08-12 01:04:21 +0700

    basic paging recyclerview done.

--- app/src/main/java/rachmanforniandi/celestialnews/ui/fragment/NewsFragment.kt
+++ app/src/main/java/rachmanforniandi/celestialnews/ui/fragment/NewsFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import android.widget.Toast
 import androidx.lifecycle.observe
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import rachmanforniandi.celestialnews.R
 import rachmanforniandi.celestialnews.adapter.NewsAdapter
 import rachmanforniandi.celestialnews.databinding.FragmentNewsBinding
@@ -21,6 +24,10 @@ class NewsFragment : Fragment(R.layout.fragment_news) {
     private lateinit var fragmentNewsBinding: FragmentNewsBinding
     private var country = "id"
     private var page = 1
+    private var pages =0
+    private var isScrolling = false
+    private var isLoading = false
+    private var isLastPage = false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -49,9 +56,14 @@ class NewsFragment : Fragment(R.layout.fragment_news) {
                     response.data?.let {
                         Log.i("MYTAG","came here ${it.articles.toList().size}")
                         newsAdapter.differ.submitList(it.articles.toList())
+                        if (it.totalResults%20 ==0){
+                            pages = it.totalResults / 20
+                        }else{
+                            pages = it.totalResults/20+1
+                        }
+                        isLastPage = page == pages
                     }
                     Log.e("testResponse",""+response.data)
-                    Log.e("response_msg_1",""+response)
                 }
                 is rachmanforniandi.celestialnews.helper.Resource.Error->{
                     hideProgressBar()
@@ -76,16 +88,45 @@ class NewsFragment : Fragment(R.layout.fragment_news) {
         fragmentNewsBinding.rvNews.apply {
 
             adapter = newsAdapter
+            //layoutManager= LinearLayoutManager(activity)
+            addOnScrollListener(this@NewsFragment.onScrollListener)
         }
         fragmentNewsBinding.rvNews.setHasFixedSize(true)
     }
 
     private fun showProgressBar(){
+        isLoading = true
         fragmentNewsBinding.pgNews.visibility = View.VISIBLE
     }
 
     private fun hideProgressBar(){
+        isLoading = false
         fragmentNewsBinding.pgNews.visibility = View.GONE
+    }
+
+    private val onScrollListener = object:RecyclerView.OnScrollListener(){
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+            if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+               isScrolling = true
+            }
+        }
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            val layoutManager = fragmentNewsBinding.rvNews.layoutManager as LinearLayoutManager
+            val sizeOfTheCurrentList = layoutManager.itemCount
+            val visibleItems = layoutManager.childCount
+            val topPosition = layoutManager.findFirstCompletelyVisibleItemPosition()
+
+            val hasReachedToEnd = topPosition+visibleItems >= sizeOfTheCurrentList
+            val shouldPaginate = !isLoading && !isLastPage && hasReachedToEnd && isScrolling
+            if (shouldPaginate){
+                page++
+                viewModel.getNewsHeadLines(country, page)
+                isScrolling = false
+            }
+        }
     }
 
 
